746f7544a918d755fea0607212e44c3377677837
Vinicius Vieira <135896031+ViniciussVieiraa@users.noreply.github.com>
2025-03-14 20:31:45 +0000

    Fiz o Código no vscode e colei no codespaces.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,91 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+       ///VARIÁVEIS
+       char Estado, Estado2;
+       char codigo_carta[50], codigo_carta2[50];
+       char nome_cidade[50], nome_cidade2[50];
+       int  populacao, ponto_turismo, populacao2, ponto_turismo2;
+       float area, area2, pib, pib2;
+   
+       /// ENTRADA DE DADOS CARTA 1
+       
+       /// Estado
+       printf("Digite o Estado da Primeira Carta: \n");
+       scanf("%s", &Estado);
+   
+       /// Codigo da Carta
+       printf("Digite o primeiro código da carta: \n");
+       scanf("%s", codigo_carta);
+       
+       /// Nome da Cidade
+       printf("Digite o nome da primeira cidade: \n");
+       fgets(nome_cidade, sizeof(nome_cidade), stdin);
+   
+       /// População
+       printf("Digite a população da primeira cidade: \n");
+       scanf("%d", &populacao);
+       
+       ///Ponto de Turismo
+       printf("Digite quantos pontos turísticos a primeira cidade vai ter: \n") ;
+       scanf("%d", &ponto_turismo);
+   
+       ///Área da cidade
+       printf("Digite quantos km vai ter a primeira cidade: \n");
+       scanf("%f", &area);
+   
+       ///Pib
+       printf("Digite o pib da primeira cidade: \n");
+       scanf("%f", &pib);
+   
+   
+   
+       //// ENTRADA DE DADOS CARTA 2
+       printf("Digite o Estado da Segunda Carta: \n");
+       scanf("%s", &Estado2);
+   
+       printf("Digite o segundo código da carta: \n");
+       scanf("%s", codigo_carta2);
+   
+       printf("Digite o nome da segunda cidade: \n");
+       fgets(nome_cidade2, sizeof(nome_cidade2), stdin);
+   
+       printf("Digite a população da segunda cidade: \n");
+       scanf("%d", &populacao2);
+   
+       printf("Digite quantos pontos turísticos a segunda cidade vai ter: \n") ;
+       scanf("%d", &ponto_turismo2);
+   
+       printf("Digite quantos km vai ter a segunda cidade: \n");
+       scanf("%f", &area2);
+   
+       printf("Digite o pib da segunda cidade: \n");
+       scanf("%f", &pib2);
+   
+   
+       ///Informações da Carta 1
+       printf("\n=== Carta 1 ===\n");
+       printf("Estado: %s \n", Estado);
+       printf("Codigo da Carta: %s \n", codigo_carta);
+       printf("Nome da Cidade: %s \n", nome_cidade);
+       printf("População: %d \n", populacao);
+       printf("Pontos Turísticos: %d \n", ponto_turismo);
+       printf("Área da Cidade: %f km²\n", area);
+       printf("Pib: %f bilhões de reais\n", pib);
+   
+       /// CARTA 2
+       
+       printf("\n=== Carta 2 ===\n");
+       printf("Estado: %s \n", Estado2);
+       printf("Codigo da Carta: %s \n", codigo_carta2);
+       printf("Nome da Cidade: %s \n", nome_cidade2);
+       printf("População: %d \n", populacao2);
+       printf("Pontos Turísticos: %d \n", ponto_turismo2);
+       printf("Área da Cidade: %f km²\n", area2);
+       printf("Pib: %f bilhões de reais\n", pib2);
+   
+   
+       return 0;
 
-    return 0;
 }
